ca4e61bd61fe1edf328e17e172e0afeba8cd59ff
AT <bigfinn82@gmail.com>
2019-11-26 21:30:13 +0400

    Add threaded simulator.

---
+++ monitoring/simulator.py
@@ -0,0 +1,47 @@
+"""Simple API testing importing the requests library."""
+import requests
+import random
+import time
+import threading
+import sys
+
+# api-endpoint
+URL = "http://localhost:5000/ping"
+
+
+def run_user(loop_max):
+    """Mock user sending requests."""
+    loop = 0
+    while loop < loop_max:
+        wait = float(random.randrange(0, 5000)) / 1000
+        time.sleep(wait)
+        r = requests.get(url=URL)
+        # extracting data in json format
+        data = r.json()
+        print data, "sleep", str(wait), "s"
+        loop = loop + 1
+
+
+def main():
+    """Main."""
+    loop_max = 10
+    if len(sys.argv) < 2:
+        users = 1
+
+    elif sys.argv[1] is not None:
+        users = sys.argv[1]
+    else:
+        users = 1
+    if len(sys.argv) > 2:
+        loop_max = sys.argv[2]
+    threads = []
+    for user in range(0, int(users)):
+        print "Starting new user", str(user)
+        thread = threading.Thread(target=run_user, args=(int(loop_max), ))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    main()
